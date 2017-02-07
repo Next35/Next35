8971b321c98c0c26b2a3ec5a9925286364cdfce5
Oscar de Leeuw <o.deleeuw@student.fontys.nl>
2017-02-07 13:14:22 +0100

    Filled in AlchemyService

--- logic/src/main/nl/next35/logic/services/AlchemyService.java
+++ logic/src/main/nl/next35/logic/services/AlchemyService.java
@@ -1,6 +1,10 @@
 package nl.next35.logic.services;
 
 import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
+import com.ibm.watson.developer_cloud.alchemy.v1.model.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 /**
@@ -9,12 +13,34 @@ import com.ibm.watson.developer_cloud.alchemy.v1.AlchemyLanguage;
 public class AlchemyService {
 
     private final AlchemyLanguage service;
+    private Map<String, Object> input;
 
     public AlchemyService() {
         this.service = new AlchemyLanguage(ServiceConstants.ALCHEMY_KEY);
     }
 
-    public AlchemyLanguage getService() {
-        return service;
+    public void setInput(String text) {
+        this.input = new HashMap<>();
+        input.put(AlchemyLanguage.TEXT, text);
+    }
+
+    public Concepts getConcepts() {
+        return service.getConcepts(input).execute();
+    }
+
+    public DocumentEmotion getEmotion() {
+        return service.getEmotion(input).execute();
+    }
+
+    public Entities getEntities() {
+        return service.getEntities(input).execute();
+    }
+
+    public Keywords getKeywords() {
+        return service.getKeywords(input).execute();
+    }
+
+    public DocumentSentiment getSentiment() {
+        return service.getSentiment(input).execute();
     }
 }
