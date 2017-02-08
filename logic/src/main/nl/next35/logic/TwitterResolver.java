896d9ba199978e78668b6ee87b29de99cbc31937
Mike Cornelissen <mike.cornelissen@student.fontys.nl>
2017-02-08 13:20:31 +0100

    implemented Twitter api functionality

--- logic/src/main/nl/next35/logic/TwitterResolver.java
+++ logic/src/main/nl/next35/logic/TwitterResolver.java
@@ -2,8 +2,9 @@ package nl.next35.logic;
 
 import nl.next35.domain.Post;
 import nl.next35.domain.User;
-import twitter4j.Twitter;
+import twitter4j.*;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,7 +13,32 @@ import java.util.List;
 public class TwitterResolver implements Resolver {
     @Override
     public List<Post> getPosts(User user) {
-        Twitter twitter =
-        return null;
+        Twitter twitter = new TwitterFactory().getInstance();
+
+        int pageno = 1;
+        List<Status> statuses = new ArrayList();
+
+        while (true) {
+            try {
+                int size = statuses.size();
+                Paging page = new Paging(pageno++, 100);
+
+                statuses.addAll(twitter.getUserTimeline(user.getUsername(), page));
+                if (statuses.size() == size) {
+                    break;
+                }
+            } catch (TwitterException e) {
+                e.printStackTrace();
+                break;
+            }
+        }
+        List<Post> posts = new ArrayList<>();
+        for (Status status : statuses) {
+            if (!status.isRetweet()) {
+                Post post = new Post(user, status.getText());
+                posts.add(post);
+            }
+        }
+        return posts;
     }
 }
--- web/src/main/nl/next35/web/Main.java
+++ web/src/main/nl/next35/web/Main.java
@@ -1,7 +1,8 @@
 package nl.next35.web;
 
-import nl.next35.logic.RedditResolver;
+import nl.next35.domain.User;
 import nl.next35.logic.ResolverChain;
+import nl.next35.logic.TwitterResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,8 +15,9 @@ public class Main {
     private static final ResolverChain chain = new ResolverChain();
 
     public static void main(String[] args) {
-        chain.add(new RedditResolver());
-
+        // chain.add(new RedditResolver());
+        chain.add(new TwitterResolver());
+        chain.resolve(new User("fastmike97"));
         SpringApplication.run(Main.class, args);
     }
 
