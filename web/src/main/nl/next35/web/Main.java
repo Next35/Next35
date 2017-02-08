dc5c8b64d091a9b28157f4ec908344755645f732
Mike Cornelissen <mike.cornelissen@student.fontys.nl>
2017-02-08 13:23:12 +0100

    implemented Twitter api functionality

--- web/src/main/nl/next35/web/Main.java
+++ web/src/main/nl/next35/web/Main.java
@@ -1,6 +1,7 @@
 package nl.next35.web;
 
 import nl.next35.domain.User;
+import nl.next35.logic.RedditResolver;
 import nl.next35.logic.ResolverChain;
 import nl.next35.logic.TwitterResolver;
 import org.springframework.boot.SpringApplication;
@@ -15,7 +16,7 @@ public class Main {
     private static final ResolverChain chain = new ResolverChain();
 
     public static void main(String[] args) {
-        // chain.add(new RedditResolver());
+        chain.add(new RedditResolver());
         chain.add(new TwitterResolver());
         chain.resolve(new User("fastmike97"));
         SpringApplication.run(Main.class, args);
